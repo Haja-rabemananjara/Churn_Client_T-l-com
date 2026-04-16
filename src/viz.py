ca83e6e62387cbe28079db196f6b44107d0d8b31
Haja <hajatiana.rabemananjara@epitech.eu>
2026-04-16 11:21:39 +0200

    feat: viz: Distribution du chrun en Camembert et barres annotées

--- src/viz.py
+++ src/viz.py
@@ -44,6 +44,7 @@ def set_project_style():
         "axes.spines.right": False,
     })
 
+
 def _save(fig: plt.Figure, filename: str, output_dir: str = "../outputs/figures"):
     """Sauvegarder un graphique si filename est fourni"""
 
@@ -53,3 +54,63 @@ def _save(fig: plt.Figure, filename: str, output_dir: str = "../outputs/figures"
         fig.savefig(path, dpi= 150, bbox_inches="tight")
         print(f"Sauvegarder: {path}")
 
+
+# 1. Distribution du churn
+def plot_churn_distribution(
+        df: pd.DataFrame,
+        col: str = "Churn",
+        save_as: str = "01_churn_distribution.png",
+) -> plt.Figure:
+    """
+    Affiche la distribution de la variable cible (camembert + barres).
+
+    Args:
+        df      : DataFrame brut (Churn en Yes/No) ou nettoyé (Churn en 0/1)
+        col     : nom de la colonne cible
+        save_as : nom du fichier de sortie (None pour ne pas sauvegarder)
+
+    Returns:
+        Figure matplolib
+    """
+    set_project_style()
+    fig, axes = plt.subplots(1, 2, figsize={12, 5})
+
+    counts = df[col].value_counts()
+    total = counts.sum()
+
+    # Camembert
+    axes[0].pie(
+        counts.values,
+        labels=[f"{idx}\n({value/total*100:.1f}%)" for idx, value in counts.items()]
+        color =[BLUE_MAIN, RED_CHURN],
+        autopct="%1.1f%%",
+        startangle=90,
+        wedgeprops={"edgecolor": "white", "linewidth":3},
+        textprops={"fontsize":11}
+    )
+    axes[0].set_title("Distribution du Churn", pad=15)
+
+    # Barres annotées
+    bar_colors=[BLUE_MAIN, RED_CHURN]
+    bars = axes[1].bar(
+        counts.index.astype(str), counts.values,
+        color=bar_colors, edgecolor="white", linewidth=2, width=0.5,
+    )
+    for bar, count in zip(bar, counts.values):
+        axes[1].text(
+            bar.get_x() + bar.get_width / 2,
+            bar.get_height() + 30,
+            f"{count:,}\nClients",
+            ha="center", fontsize=10, fontweight="bold",
+        )
+    axes[1].set_title("Nombre de clients par groupe")
+    axes[1].set_ylabel("Nombre de clients")
+    axes[1].set_ylim(0, counts.max() *1.2)
+
+    fig.suptitle(
+        f"Vue d'ensemble : Churn Cient Télécom (n={total:,})",
+        fontsize=15, fontweight="bold", y=1.02
+    )
+    plt.tight_layout()
+    _save(fig, save_as)
+    return fig
